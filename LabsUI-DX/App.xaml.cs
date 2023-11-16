a71142fcb2c3c3bb7264ec3cd825c0f20b8ec6c3
Nils-Olof Bengtsson <nils-olof.bengtsson@cgi.com>
2023-11-16 09:53:14 +0100

    Added use of OnStartup

--- LabsUI-DX/App.xaml.cs
+++ LabsUI-DX/App.xaml.cs
@@ -13,6 +13,15 @@ namespace LabsUI
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Instead of StartupUri="Views/MainWindow.xaml", use OnStartup to prepare for Dependency Injection..
+            MainWindow win = new MainWindow();
+            win.Show();
+
+            base.OnStartup(e);
+        }
+
         private void Application_Exit(object sender, ExitEventArgs e)
         {
         }
